a783dbb1baed8ab251c6e1760d9d34afc7a06ac4
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 07:08:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,5 +26,5 @@ function incrementVariable() {
 function doWhileLoop(n) {
   do {
     console.log("I run once regardless.")
-  } while (incrementVariable() < n +1 )
+  } while (incrementVariable() < n + 1 )
 }
